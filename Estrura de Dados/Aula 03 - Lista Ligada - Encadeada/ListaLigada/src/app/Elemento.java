1ab193abcc4b3735742a88f599fc83dce8876354
Alexandre Eustaquio <alexeus.souza@gmail.com>
2023-02-06 08:05:47 -0300

    update lista

---
+++ Estrura de Dados/Aula 03 - Lista Ligada - Encadeada/ListaLigada/src/app/Elemento.java
@@ -0,0 +1,27 @@
+package app;
+
+public class Elemento {
+	private String valor;
+	private Elemento proximo;
+	
+	
+	public Elemento(String valor) {
+		this.valor = valor;
+	}
+	
+	public String getValor() {
+		return valor;
+	}
+	
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	public Elemento getProximo() {
+		return proximo;
+	}
+	
+	public void setProximo(Elemento proximo) {
+		this.proximo = proximo;
+	}
+}
